d80dd4d4807e1b29928a6a2af63d94e5ffb167c2
Eshwar Reddy <eshwarreddy.thummala@gmail.com>
2024-02-20 20:36:08 -0700

    Completed set up for new component post

--- src/components/UserModal.jsx
+++ src/components/UserModal.jsx
@@ -1,13 +1,19 @@
 import Modal from "react-bootstrap/Modal";
+import UserPost from "./UserPost";
 
 export default function UserModal({ show, setShow, userData}) {
   console.log(show +"   in modal")
+  const { user_profile, posts_info } = userData
   return (
     <Modal show={show} fullscreen={true} onHide={() => setShow(false)}>
       <Modal.Header closeButton>
-        <Modal.Title>Modal</Modal.Title>
+        <Modal.Title>More Posts of {user_profile.name}</Modal.Title>
       </Modal.Header>
-      <Modal.Body>Modal body content</Modal.Body>
+      <Modal.Body>
+        {
+            posts_info.map(post => <UserPost key={post.post_uid} post={post}/>)
+        }
+      </Modal.Body>
     </Modal>
   );
 }
---
+++ src/components/UserPost.jsx
@@ -0,0 +1,6 @@
+
+export default function UserPost({ post }){
+    return <div>
+        
+    </div>
+}
